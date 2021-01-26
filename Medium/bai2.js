e8c4a8633942469023ffa59f6f922092a0a89994
LeNam97 <namle8070@gmail.com>
2021-01-26 18:10:39 +0700

    Medium sheet

--- Medium/bai2.js
+++ Medium/bai2.js
@@ -1,13 +1,11 @@
 
 function joinArray(arrayOne, arrayTwo){
   var array = [''];
-
   var arrayThree= arrayOne.concat(arrayTwo);
   for(var i = 0; i < arrayThree.length; i++){
     array[i] = arrayThree[i];
   }
-  return array;
-  
+  return array; 
 }
 module.exports = {
   joinArray
--- Medium/bai7.js
+++ Medium/bai7.js
@@ -1,11 +1,7 @@
 function sliceArray(array,start,end){
-
   var newArrays = [""];
-
   newArrays = array.slice(start,end);
-
   return newArrays;
-
 }
 module.exports = {
   sliceArray
